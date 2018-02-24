5d804e38ade7cf64db7745e89bdb9814b8e0ea35
sl0wik <slowik.michael@gmail.com>
2018-02-24 17:19:00 +0100

    Content controller

---
+++ src/Http/Controller/ContentController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Sl0wik\Webset\Http\Controllers;
+
+use Sl0wik\Webset\Models\Content;
+use Sl0wik\Webset\Models\Menu;
+use Illuminate\Http\Request;
+use Mcamara\LaravelLocalization\LaravelLocalization;
+
+class ContentController extends Controller
+{
+    public function show(Content $content)
+    {
+        return view('pages.content', ['content' => $content]);
+    }
+
+    public function showBySlug(Request $request)
+    {
+        $uri = str_replace(LaravelLocalization::getCurrentLocale().'/', '', $request->route()->uri);
+
+    	$content = Content::where([
+    		['url_path', $uri],
+    		['language_code', LaravelLocalization::getCurrentLocale()]
+    	]);
+
+        return view('pages.content', ['content' => $content->firstOrFail()]);
+    }
+}
