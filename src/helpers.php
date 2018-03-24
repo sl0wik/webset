900f76ce68c6baf98b21b09361e69c2155a772bb
sl0wik <slowik.michael@gmail.com>
2018-03-24 13:58:56 +0100

    __t helper for translations

--- src/helpers.php
+++ src/helpers.php
@@ -1,6 +1,34 @@
 <?php
 
 /**
+ * Short function to return key from dictionary. Laravel trans() helper behing.
+ *
+ * @param string          $key      Key name like "dict.test".
+ * @param string|null     $data     Optional parameters to trans.
+ * @return string
+ */
+function __t($key, $data = null)
+{
+    if (\Lang::has($key)) {
+        if ($data) {
+            return trans($key, $data);
+        } else {
+            return trans($key);
+        }
+    } else {
+        // TODO: we need parsing like in trans
+        $translation = Sl0wik\Webset\Models\Translation::getCached();
+        $item = $translation->where('language_code', \LaravelLocalization::getCurrentLocale())->where('key', $key)->first();
+
+        if ($item) {
+            return $item->value;
+        } else {
+            return $key;
+        }
+    }
+}
+
+/**
  * Returning translated (lozalized) url.
  *
  * @param string      $url        url for example /guides/{name}
